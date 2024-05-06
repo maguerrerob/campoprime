d8648f92c3dd83fb59b892d139548e875a742486
maguerrerob <manuel.guerrero.boza.al@iespoligonosur.org>
2024-05-06 23:10:16 +0200

    login and register frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegistroComponent } from './registro/registro.component';
+
+const routes: Routes = [
+  { path: "", component: LoginComponent },
+  { path: "registro", component: RegistroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
